d3bd112e8cd0079d1c9af9038853858d091f8209
abugi <abugiboi@gmail.com>
2017-10-27 23:19:05 +0100

    initial commit

--- syntaxChanges/closure.js
+++ syntaxChanges/closure.js
@@ -41,4 +41,31 @@ setTimeout( function timer(){
 console.log( message );
 }, 1000 );
 }
-wait( "Hello, closure!" );
+wait( "Hello, closure!"); 
+
+//loops and closure
+/* 
+	
+*/
+for (var i=1; i<=5; i++) {
+setTimeout( function timer(){
+console.log( i );
+}, i*1000 );
+}
+
+for (var i=1; i<=5; i++) {
+(function(){
+setTimeout( function timer(){
+console.log( i );
+}, i*1000 );
+})();
+}
+
+for (var i=1; i<=5; i++) {
+(function(){
+var j = i;
+setTimeout( function timer(){
+console.log( j );
+}, j*1000 );
+})();
+}
